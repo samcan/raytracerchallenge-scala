7a738296c96c8f4b0bb230cd3c076f4db6dd71af
Sam Cantrell <1220819+samcan@users.noreply.github.com>
2025-04-29 20:50:21 -0500

    Add some operations on Color

--- src/main/scala/Color.scala
+++ src/main/scala/Color.scala
@@ -1,1 +1,17 @@
 case class Color(red: Double, green: Double, blue: Double)
+
+def isEqual(a: Color, b: Color): Boolean =
+  almostEqual(a.red, b.red) && almostEqual(a.green, b.green) &&
+    almostEqual(a.blue, b.blue)
+
+def add(a: Color, b: Color): Color =
+  Color(a.red + b.red, a.green + b.green, a.blue + b.blue)
+
+def subtract(a: Color, b: Color): Color =
+  Color(a.red - b.red, a.green - b.green, a.blue - b.blue)
+
+def multiply(a: Color, b: Double): Color =
+  Color(a.red * b, a.green * b, a.blue * b)
+
+def multiply(a: Color, b: Color): Color =
+  Color(a.red * b.red, a.green * b.green, a.blue * b.blue)
--- src/test/scala/ColorSuite.scala
+++ src/test/scala/ColorSuite.scala
@@ -5,4 +5,49 @@ class ColorSuite extends munit.FunSuite {
     assertEquals(obtained.green, 0.4)
     assertEquals(obtained.blue, 1.7)
   }
+
+  test("Adding colors") {
+    val obtained = add(Color(0.9, 0.6, 0.75), Color(0.7, 0.1, 0.25))
+    val expected = Color(1.6, 0.7, 1.0)
+    assertEquals(isEqual(obtained, expected), true)
+  }
+
+  test("Subtracting colors") {
+    val obtained = subtract(Color(0.9, 0.6, 0.75), Color(0.7, 0.1, 0.25))
+    val expected = Color(0.2, 0.5, 0.5)
+    assertEquals(isEqual(obtained, expected), true)
+  }
+
+  test("Multiplying a color by a scalar") {
+    val obtained = multiply(Color(0.2, 0.3, 0.4), 2)
+    val expected = Color(0.4, 0.6, 0.8)
+    assertEquals(isEqual(obtained, expected), true)
+  }
+
+  test("Multiplying colors") {
+    val obtained = multiply(Color(1, 0.2, 0.4), Color(0.9, 1, 0.1))
+    val expected = Color(0.9, 0.2, 0.04)
+    assertEquals(isEqual(obtained, expected), true)
+  }
+
+  test("isEqual() returns true for matching colors") {
+    val a = Color(3.3, 4.5, -1.2)
+    val b = Color(3.3, 4.5, -1.2)
+
+    assertEquals(isEqual(a, b), true)
+  }
+
+  test("isEqual() returns true for floating-point error colors") {
+    val a = Color(3.3, 4.5, -1.2)
+    val b = Color(3.3000000001, 4.500000000005, -1.200000003)
+
+    assertEquals(isEqual(a, b), true)
+  }
+
+  test("isEqual() returns false for non-matching colors") {
+    val a = Color(3.3, 4.5, -1.2)
+    val b = Color(5.9, 4.5, -1.2)
+
+    assertEquals(isEqual(a, b), false)
+  }
 }
