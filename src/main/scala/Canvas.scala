ed7fdf86c21960f2cc56415e7af006d0f171e7a6
Sam Cantrell <1220819+samcan@users.noreply.github.com>
2025-05-03 13:20:48 -0500

    Add more tests of Canvas

--- src/main/scala/Canvas.scala
+++ src/main/scala/Canvas.scala
@@ -2,6 +2,22 @@ package com.samuelcantrell.raytracer.canvas
 
 import com.samuelcantrell.raytracer.color
 
-class Canvas(var width: Int, var height: Int):
-  private var _data = Array.ofDim[color.Color](height, width)
+class Canvas(
+    var width: Int,
+    var height: Int,
+    c: color.Color = color.Color(0, 0, 0)
+):
+  private var _data =
+    Array.fill[color.Color](height, width)(c)
+
+  def writePixel(x: Int, y: Int, c: color.Color): Unit = {
+    _data(y)(x) = c
+  }
+
+  def pixelAt(x: Int, y: Int): color.Color = {
+    _data(y)(x)
+  }
+
+  def rows(): Int = _data.length
+  def cols(): Int = _data(0).length
 end Canvas
--- src/test/scala/CanvasSuite.scala
+++ src/test/scala/CanvasSuite.scala
@@ -2,11 +2,47 @@ import com.samuelcantrell.raytracer.canvas._
 import com.samuelcantrell.raytracer.color
 
 class CanvasSuite extends munit.FunSuite {
+  val red = color.Color(1, 0, 0)
+  val black = color.Color(0, 0, 0)
+
   test("Creating a canvas") {
     val c = Canvas(10, 20)
     assertEquals(c.width, 10)
     assertEquals(c.height, 20)
 
-    // TODO: check that Canvas has only colors of (0, 0, 0)
+    for {
+      i <- 0 until 20
+      j <- 0 until 10
+    } assertEquals(c.pixelAt(j, i), black)
+  }
+
+  test("Check that canvas has correct number of data rows") {
+    val c = Canvas(10, 20)
+    assertEquals(c.rows(), 20)
+  }
+
+  test("Check that canvas has correct number of data cols") {
+    val c = Canvas(10, 20)
+    assertEquals(c.cols(), 10)
+  }
+
+  test("Creating a canvas with a non-default color") {
+    val c = Canvas(10, 20, color.Color(0.1, 0.2, 0.3))
+
+    for {
+      i <- 0 until 20
+      j <- 0 until 10
+    } assertEquals(c.pixelAt(j, i), color.Color(0.1, 0.2, 0.3))
+  }
+
+  test("Check a pixel with pixelAt()") {
+    val c = Canvas(10, 20)
+    assertEquals(c.pixelAt(2, 1), black)
+  }
+
+  test("Writing pixels to a canvas") {
+    val c = Canvas(10, 20)
+    c.writePixel(2, 3, red)
+    assertEquals(c.pixelAt(2, 3), red)
   }
 }
