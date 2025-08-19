6cf5dbc4dc8cd7c81a4debb040935e5ecc86784b
Sam Cantrell <1220819+samcan@users.noreply.github.com>
2025-08-19 11:35:16 -0700

    Add tests for cylinder length
    (Implemented using Anthropic's Claude)

--- src/test/scala/CylinderSuite.scala
+++ src/test/scala/CylinderSuite.scala
@@ -5,6 +5,13 @@ import com.samuelcantrell.raytracer.equality
 
 class CylinderSuite extends munit.FunSuite {
 
+  test("The default minimum and maximum for a cylinder") {
+    val cyl = cylinder()
+    
+    assertEquals(cyl.minimum, Double.NegativeInfinity)
+    assertEquals(cyl.maximum, Double.PositiveInfinity)
+  }
+
   test("A ray misses a cylinder - test 1") {
     val cyl = cylinder()
     val direction = tuple.normalize(tuple.makeVector(0, 1, 0))
@@ -95,5 +102,59 @@ class CylinderSuite extends munit.FunSuite {
     val expected = tuple.makeVector(-1, 0, 0)
 
     assertEquals(tuple.isEqual(n, expected), true)
+  }
+
+  test("Intersecting a constrained cylinder - test 1") {
+    val cyl = cylinder().copy(minimum = 1, maximum = 2)
+    val direction = tuple.normalize(tuple.makeVector(0.1, 1, 0))
+    val r = ray.ray(tuple.makePoint(0, 1.5, 0), direction)
+    val xs = localIntersect(cyl, r)
+
+    assertEquals(xs.count, 0)
+  }
+
+  test("Intersecting a constrained cylinder - test 2") {
+    val cyl = cylinder().copy(minimum = 1, maximum = 2)
+    val direction = tuple.normalize(tuple.makeVector(0, 0, 1))
+    val r = ray.ray(tuple.makePoint(0, 3, -5), direction)
+    val xs = localIntersect(cyl, r)
+
+    assertEquals(xs.count, 0)
+  }
+
+  test("Intersecting a constrained cylinder - test 3") {
+    val cyl = cylinder().copy(minimum = 1, maximum = 2)
+    val direction = tuple.normalize(tuple.makeVector(0, 0, 1))
+    val r = ray.ray(tuple.makePoint(0, 0, -5), direction)
+    val xs = localIntersect(cyl, r)
+
+    assertEquals(xs.count, 0)
+  }
+
+  test("Intersecting a constrained cylinder - test 4") {
+    val cyl = cylinder().copy(minimum = 1, maximum = 2)
+    val direction = tuple.normalize(tuple.makeVector(0, 0, 1))
+    val r = ray.ray(tuple.makePoint(0, 2, -5), direction)
+    val xs = localIntersect(cyl, r)
+
+    assertEquals(xs.count, 0)
+  }
+
+  test("Intersecting a constrained cylinder - test 5") {
+    val cyl = cylinder().copy(minimum = 1, maximum = 2)
+    val direction = tuple.normalize(tuple.makeVector(0, 0, 1))
+    val r = ray.ray(tuple.makePoint(0, 1, -5), direction)
+    val xs = localIntersect(cyl, r)
+
+    assertEquals(xs.count, 0)
+  }
+
+  test("Intersecting a constrained cylinder - test 6") {
+    val cyl = cylinder().copy(minimum = 1, maximum = 2)
+    val direction = tuple.normalize(tuple.makeVector(0, 0, 1))
+    val r = ray.ray(tuple.makePoint(0, 1.5, -2), direction)
+    val xs = localIntersect(cyl, r)
+
+    assertEquals(xs.count, 2)
   }
 }
